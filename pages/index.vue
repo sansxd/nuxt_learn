934ff2eca9b0cded78cb7c182f902b22b645bb5c
Sergio Nuñez <sergioans95@hotmail.com>
2020-02-11 17:05:21 -0300

    add heroku Procfile

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,48 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        aprendiendo_nuxt
-      </h1>
-      <h2 class="subtitle">
-        aprender sobre nuxt y todo esoo
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <h2>Users</h2>
+    <ul class="users">
+      <li v-for="user in users" :key="user.id">
+        <NuxtLink :to="'/users/' + user.id">
+          {{ user.name }}
+        </NuxtLink>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import axios from 'axios'
 
 export default {
-  components: {
-    Logo
+  async asyncData() {
+    const { data } = await axios.get(
+      'https://jsonplaceholder.typicode.com/users'
+    )
+    return { users: data }
   }
 }
 </script>
 
-<style>
+<style scoped>
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   text-align: center;
+  margin-top: 100px;
+  font-family: sans-serif;
 }
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.users {
+  list-style-type: none;
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.users li a {
+  display: inline-block;
+  width: 200px;
+  border: 1px #ddd solid;
+  padding: 10px;
+  text-align: left;
+  color: #222;
+  text-decoration: none;
 }
-
-.links {
-  padding-top: 15px;
+.users li a:hover {
+  color: orange;
 }
 </style>
